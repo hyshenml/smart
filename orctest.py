6fab5b2609d068a8211ffb16df0789a65ef6b5c7
hyshenml <hyshenml@gmail.com>
2018-01-27 22:15:45 +0800

    2123

--- orctest.py
+++ orctest.py
@@ -1,3 +1,26 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'sml'
+from aip import AipOcr
+
+""" 你的 APPID AK SK """
+APP_ID = '10758323'
+API_KEY = 'q0zI5K7I51EDKNlr0RR0dD1L'
+SECRET_KEY = '0HQy36VkQsen7Pmhqg3RRikG1nZyDqmA'
+
+client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+image = get_file_content('example.jpg')
+options = {}
+options["language_type"] = "CHN_ENG"
+options["detect_direction"] = "true"
+options["detect_language"] = "true"
+options["probability"] = "true"
+""" 带参数调用通用文字识别, 图片参数为本地图片 """
+res=client.basicGeneral(image, options)
+for word in res['words_result']:
+    print word['words']
